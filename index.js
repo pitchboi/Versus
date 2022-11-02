becbc24acd314b81dfb558332ffbf02b5766d0b8
pitchboi <113968796+pitchboi@users.noreply.github.com>
2022-11-02 11:58:05 -0700

    Fixed scoring algorithm

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ let inputString = "";
 let inputArray = [];
 let arenaArray = [];
 let resultArray = [];
+let tempArray = [];
 let round = 0;
+let iteration = 0;
+let matches = 1;
 let firstButtonNumber = 0;
 let secondButtonNumber = 0;
 let allInPlay = false;
@@ -52,6 +55,7 @@ function createArena() {
         resultArray.push(0); // Make entry in result array
     }
     resultArray.push(0); // Make final entry in result array (since the loop stops 1 short of total entries)
+    tempArray = JSON.parse(JSON.stringify(resultArray));
 }
 
 function nextMatch() {
@@ -60,6 +64,12 @@ function nextMatch() {
     secondButtonNumber = arenaArray[round][1];
     elFirstButton.textContent = inputArray[firstButtonNumber];
     elSecondButton.textContent = inputArray[secondButtonNumber];
+
+    // Check if part of a new iteration
+    if (firstButtonNumber !== iteration) {
+        iteration += 1;
+        tempArray = JSON.parse(JSON.stringify(resultArray));
+    }
 
     // Bump the round number up to reflect what round we're on (starts at 1, not 0)
     round += 1;
@@ -70,13 +80,17 @@ function nextMatch() {
     }
 
     // Display test totals **************************************** FOR DEBUG ****************************************
-    elUl.innerHTML = "";
+    /*elUl.innerHTML = "";
     for (i = 0; i < resultArray.length; i++) {
         elUl.innerHTML += `<li>${inputArray[i]}: ${resultArray[i]}</li>`;
-    }
+    }*/
+    /*elUl.innerHTML += `<li>-------------------</li>`
+    for (i = 0; i < resultArray.length; i++) {
+        elUl.innerHTML += `<li>${inputArray[i]}: ${tempArray[i]}</li>`;
+    }*/
 
     // Check if first and second numbers are unequal, and as long as all items on list are 'in play', the matchup can be skipped because the answer is already known
-    if (resultArray[firstButtonNumber] !== resultArray[secondButtonNumber] && allInPlay) {
+    if (tempArray[firstButtonNumber] !== tempArray[secondButtonNumber]) {
         if (round === arenaArray.length) {
             endGame(); // End the game, rounds are all done!
         } else {
@@ -89,37 +103,20 @@ function clickButton(buttonNumber) {
     // Deduct one point from the loser's slot in the result array
     let temp = 0;
     let otherTemp = 0;
-    let otherOtherTemp = 0;
 
     if (buttonNumber === 1) { // First button
-        //if (resultArray[firstButtonNumber] - 1 < resultArray[secondButtonNumber] - 1) {
-            resultArray[secondButtonNumber] = resultArray[firstButtonNumber] - 1;
-            temp = resultArray[secondButtonNumber];
-        /*} else if (resultArray[firstButtonNumber] - 1 >= resultArray[secondButtonNumber] - 1) {
-            resultArray[secondButtonNumber] = resultArray[secondButtonNumber] - 1;
-            temp = resultArray[secondButtonNumber] + 1;
-        }*/ /*else {
-            resultArray[secondButtonNumber] = resultArray[secondButtonNumber] - 1;
-            temp = resultArray[secondButtonNumber];
-        }*/
+        resultArray[secondButtonNumber] = resultArray[firstButtonNumber] - 1;
+        temp = resultArray[secondButtonNumber];
         otherTemp = secondButtonNumber;
     } else { // Second button
-        //if (resultArray[secondButtonNumber] - 1 < resultArray[firstButtonNumber] - 1) {
-            resultArray[firstButtonNumber] = resultArray[secondButtonNumber] - 1;
-            temp = resultArray[firstButtonNumber];
-        /*} else if (resultArray[secondButtonNumber] - 1 >= resultArray[firstButtonNumber] - 1) {
-            resultArray[firstButtonNumber] = resultArray[firstButtonNumber] - 1;
-            temp = resultArray[firstButtonNumber] + 1;
-        }*/ /*else {
-            resultArray[firstButtonNumber] = resultArray[firstButtonNumber] - 1;
-            temp = resultArray[firstButtonNumber];
-        }*/
+        resultArray[firstButtonNumber] = resultArray[secondButtonNumber] - 1;
+        temp = resultArray[firstButtonNumber];
         otherTemp = firstButtonNumber;
     }
 
     // Bump lower losers down a peg
     for (i = 0; i < resultArray.length; i++) {
-        if (resultArray[i] < temp && i !== otherTemp) {
+        if (tempArray[i] <= temp && i !== otherTemp) {
             resultArray[i] -= 1;
         }
     }
@@ -132,6 +129,8 @@ function clickButton(buttonNumber) {
         // Generate next match
         nextMatch();
     }
+
+    matches += 1;
 }
 
 function endGame() {
